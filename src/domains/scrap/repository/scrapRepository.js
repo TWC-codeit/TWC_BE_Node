56b995cba4550326fd3a8a4c691fb3da6c7e98dd
HSK021843 <9902kim@hufs.ac.kr>
2025-01-30 19:52:34 +0900

    refactor: Repository 추가
    Repository 레이어를 추가해서 코드를 분리함

---
+++ src/domains/scrap/repository/scrapRepository.js
@@ -0,0 +1,32 @@
+const {prisma} = require("../../../config/db");
+
+const getAllScraps = async (userId) => {
+    const scraps = await prisma.scraps.findMany({
+        where: {
+            userId: userId,
+        },
+
+        include: {
+            articles: {
+                select: {
+                    title: true,
+                },
+            },
+        },
+
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+
+    return scraps.map(scrap => ({
+        id: scrap.id,
+        articleId: scrap.articleId,
+        title: scrap.articles.title,
+        scrapedAt: scrap.createdAt,
+    }));
+}
+
+module.exports = {
+    getAllScraps,
+};
--- src/domains/scrap/service/scrapService.js
+++ src/domains/scrap/service/scrapService.js
@@ -1,31 +1,8 @@
-const { prisma } = require('../../../config/db');
+const scrapRepository = require('../repository/scrapRepository');
 
 const getAllScrap = async (userId) => {
-    const scraps = await prisma.scraps.findMany({
-        where: {
-            userId: userId,
-        },
-
-        include: {
-            articles: {
-                select: {
-                    title: true,
-                },
-            },
-        },
-
-        orderBy: {
-            createdAt: 'desc',
-        },
-    });
-
-    return scraps.map(scrap => ({
-        id: scrap.id,
-        articleId: scrap.articleId,
-        title: scrap.articles.title,
-        scrapedAt: scrap.createdAt,
-    }));
-}
+    return await scrapRepository.getAllScraps(userId);
+};
 
 module.exports = {
     getAllScrap,

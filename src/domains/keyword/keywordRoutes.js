c9bec977f49ade0089b7477d7fe82745d399ab46
jinsoeun <rosejinse@naver.com>
2025-01-31 13:27:17 +0900

    fix: 기사 목록 조회 API 엔드포인트 변경

--- src/domains/keyword/keywordRoutes.js
+++ src/domains/keyword/keywordRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get('/articles/count/:keyword', getArticleCounts);
 
 // 특정 키워드를 포함한 기사 목록 조회
-router.get('/articles/keywords/:keyword', getArticlesByKeyword);
+router.get('/articles/:keyword', getArticlesByKeyword);
 
 module.exports = router;
